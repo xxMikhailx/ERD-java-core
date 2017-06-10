6dd79190f499a6aa1eb4621317cc1bb10bd12bc9
Mikhail Kaziuchyts <mikhail_kaziuchyts@epam.com>
2017-06-10 16:23:57 +0300

    Packages, jars. Running and compiling.

---
+++ PackageNJar/src/com/epam/example1/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.epam.example1.hello;
+
+import com.epam.example1.world.World;
+
+public class Hello{
+	public static void printHello(){
+		System.out.print("Hello ");
+		World.printWorld();
+	}
+}
---
+++ PackageNJar/src/com/epam/example1/world/World.java
@@ -0,0 +1,7 @@
+package com.epam.example1.world;
+
+public class World{
+	public static void printWorld(){
+		System.out.println("world!");
+	}
+}
